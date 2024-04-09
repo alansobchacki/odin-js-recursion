c1e9a913e09c40ae8656ad4e47150c0f577c0d0f
Alan Sobchacki <53805387+alansobchacki@users.noreply.github.com>
2024-04-09 19:38:06 -0300

    Refactor mergeSort function
    mergeSort() now has proper merge sort methodology (old function still included for comedy purposes)

--- merge.js
+++ merge.js
@@ -1,6 +1,43 @@
 // Build a function mergeSort that takes in an array 
 // and returns a sorted array, using a recursive merge sort methodology
 
+function merge(left, right) {
+  let arr = []
+
+  while (left.length && right.length) {
+      if (left[0] < right[0]) {
+          arr.push(left.shift())  
+      } else {
+          arr.push(right.shift()) 
+      }
+  }
+  
+  return [ ...arr, ...left, ...right ]
+}
+
+function mergeSort(array) {
+  const half = array.length / 2
+  
+  if(array.length < 2){
+    return array 
+  }
+  
+  const left = array.splice(0, half)
+  return merge(mergeSort(left),mergeSort(array))
+}
+
+// An input of [3, 2, 1, 13, 8, 5, 0, 1] should return [0, 1, 1, 2, 3, 5, 8, 13] 
+// And an input of [105, 79, 100, 110] should return [79, 100, 105, 110].
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1], []));
+console.log(mergeSort([105, 79, 100, 110], []));
+
+// My initial approach to the problem
+// I forgot I had to use merge sort methodology
+// But I liked my solution so it's also here:
+
+/*
+
 function mergeSort(array, sortedArray) {
   if (array.length < 1) {
     return sortedArray;
@@ -12,10 +49,5 @@ function mergeSort(array, sortedArray) {
 
   sortedArray.push(sortedNumber);
   return mergeSort(array, sortedArray);
-}
 
-// An input of [3, 2, 1, 13, 8, 5, 0, 1] should return [0, 1, 1, 2, 3, 5, 8, 13] 
-// And an input of [105, 79, 100, 110] should return [79, 100, 105, 110].
-
-console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1], []));
-console.log(mergeSort([105, 79, 100, 110], []));
+  */
